f11a9f7ee9e9f39ebeac78adb3843bf7a53a1b8e
popka <magemaster555@gmail.com>
2022-10-09 00:21:34 +0300

    fuck

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 from core import LongPoll
-import os, handler, sys, importlib
-
-importlib.reload(sys)
-sys.setdefaultencoding('utf8')
+import os, handler
 
 LP = LongPoll(os.getenv("VK_TOKEN"))
 LP.addListener('message_new', handler.new_message)
