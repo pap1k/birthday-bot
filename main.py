0f1d913246b60fbc931175fe5b84f1f17f4ce254
popka <magemaster555@gmail.com>
2022-10-09 00:16:44 +0300

    enc fix mb

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from core import LongPoll
-import os, handler
+import os, handler, sys
+
+sys.setdefaultencoding('utf8')
 
 LP = LongPoll(os.getenv("VK_TOKEN"))
 LP.addListener('message_new', handler.new_message)
